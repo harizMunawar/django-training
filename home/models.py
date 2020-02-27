885e460431b631327b813dba0e3d4917ec8a021a
harizMunawar <munawarhariz@gmail.com>
2020-02-28 00:00:25 +0700

    bismillah ada bintang

--- home/models.py
+++ home/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.contenttypes.fields import GenericRelation
-from django.contrib.contenttypes.models import Rating
 
 class menu(models.Model):
     menu_name = models.CharField(max_length=50)
