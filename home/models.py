5ae40d3faf67bffda01aa95c1275a6b74d02a0a7
harizMunawar <munawarhariz@gmail.com>
2020-02-27 23:36:47 +0700

    bismillah ada bintang

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ class menu(models.Model):
     menu_name = models.CharField(max_length=50)
     price = models.IntegerField()
     image = models.ImageField(upload_to='uploads/%Y/%M/%D', default='img/ahiface.png')
+    ratings =  GenericRelation(Rating, related_query_name= 'object_list')
     slug = models.SlugField(blank=True, editable=False)
 
     def save(self):
